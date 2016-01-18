3f28dd71f8a3eeb980b88367e3e8f5c17847912e
yimouleng <wmt443040748@163.com>
2016-01-18 15:06:57 +0800

    Initial Commit

---
+++ MTTouchIdDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTTouchIdDemo
+//
+//  Created by Eli on 16/1/18.
+//  Copyright © 2016年 Ely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTTouchIdDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTTouchIdDemo
+//
+//  Created by Eli on 16/1/18.
+//  Copyright © 2016年 Ely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
